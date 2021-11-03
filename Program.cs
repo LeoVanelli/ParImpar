e0664cca9d3712e5f70937ed1901f43fb0378e5c
Leandro Gonçalves Vanelli <55560279+LeoVanelli@users.noreply.github.com>
2021-11-03 20:00:28 -0300

    Adiciona atividade ParImpar

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ParImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número: ");
+            double _numero = double.Parse(Console.ReadLine());
+            string _validador = _numero % 2 == 0 ? "Par" : "Impar";
+
+            Console.WriteLine($"{_numero} é {_validador}");
+        }
+    }
+}
